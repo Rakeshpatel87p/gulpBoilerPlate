1a8b38be9d8d5795c4af25af8c5d8e3b38bbce23
Rakesh Patel <rakesh.patel@accesscfa.com>
2019-06-02 09:27:11 -0400

    watch status working

--- gulpfile.js
+++ gulpfile.js
@@ -20,9 +20,9 @@ gulp.task('sass', function(cb) {
 	return gulp.src('app/scss/main.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('app/css'))
-		cb() //close async function. Otherwise throws err at not having completed
+		//cb() //close async function. Otherwise throws err at not having completed
 });
 
-gulp.task('watch', ['sass'], function(){
-	gulp.watch('app/scss/**/.scss', ['sass']);
+gulp.task('watch', function(){
+	gulp.watch('app/scss/**/*.scss', ['sass']);
 });
